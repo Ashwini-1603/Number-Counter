2bbee1f66eb67683a2fa1cadf34fb0f38a3e88c7
Ashwini-1603 <106587281+Ashwini-1603@users.noreply.github.com>
2024-04-02 13:24:58 +0530

    Update project with new changes

--- src/Component/Counter.js
+++ src/Component/Counter.js
@@ -5,11 +5,21 @@ import{faTrash,faPause ,faPlay} from "@fortawesome/free-solid-svg-icons"
             const [count, setCount] = useState(0);
           const [paused, setPaused] = useState(false);
         const [checkPosition,setCheckPosition]=useState(true)
-        const [timer,setTimer]=useState("")
+        const [timer,setTimer]=useState("");
+        const [started, setStarted] = useState(false);
+        const [incrementHandle, setIncrementHandle] = useState(false);
+
+        //set started
+        const startCounter = () => {
+          setStarted(true);
+        
+        };
           // Function to increment the count
         
           function incrementCount(){
             setCheckPosition(true)
+            setIncrementHandle(false)
+            startCounter()
             clearInterval(timer)
             setPaused(false)
            const d= setInterval(() => {
@@ -20,6 +30,8 @@ import{faTrash,faPause ,faPlay} from "@fortawesome/free-solid-svg-icons"
         
          // Function to decrement the count
         function decrementCount(){
+          
+          setIncrementHandle(true)
           setPaused(false)
           setCheckPosition(false)
             clearInterval(timer)
@@ -44,14 +56,15 @@ import{faTrash,faPause ,faPlay} from "@fortawesome/free-solid-svg-icons"
         }
      
           return (
-            <div className='counter-container bg-success bg-opacity-25 rounded-1'>  
+            <div className='counter-container  rounded-1 '>  
             <div className='inner-container'>
             <h1> {count}</h1>
-            <button className='m-2 px-2 btn btn-success'  onClick={incrementCount}>Start</button>
+            <button className='m-2 px-2 btn btn-success'  style={{ display: started ? 'none' : 'inline' }} onClick={incrementCount } >Start</button>
             {/* <button onClick={togglePause}>Pause</button> */}
-            <button className='m-2 px-2 btn btn-warning btn-style'  onClick={togglePause}>{paused ? <FontAwesomeIcon icon={faPlay} /> : <FontAwesomeIcon icon={faPause} />}</button>
-            <button className='m-1 px-1 btn btn-secondary btn-style'  onClick={incrementCount}>+</button>
-            <button className='m-1 px-1 btn btn-secondary btn-style'  onClick={decrementCount}>-</button>
+            <button className='m-2 px-2 btn btn-warning btn-style' style={{color:"white"}} onClick={togglePause}>{paused ? <FontAwesomeIcon icon={faPlay} /> : <FontAwesomeIcon icon={faPause} />}</button>
+            {/* <button className='m-1 px-1 btn btn-secondary btn-style'  onClick={incrementCount}>+</button>
+            <button className='m-1 px-1 btn btn-secondary btn-style'  onClick={decrementCount}>-</button> */}
+            <button className='m-1 px-1 btn btn-secondary btn-style'  onClick={incrementHandle? incrementCount:decrementCount}>{incrementHandle?"+":"-"}</button>
             <button className='m-2 px-2 btn btn-danger btn-style'  onClick={onDelete}><FontAwesomeIcon icon={faTrash} /></button>
           
           </div></div>
